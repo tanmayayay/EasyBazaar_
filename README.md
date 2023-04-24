e271dbe0117887f7113d5626bb727efc2c7f2374
Tanmay Singh <tanmayayay@tanmays-mbp.myfiosgateway.com>
2023-04-24 10:43:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# E-Commerce Website
+
+Easy Bazaar is a E-commerce web application made with React.js. It is an intuitive online shopping platform which is designed to simplify the users experience by offering the users persinalized recommendations, a user friendly interface and a streamlined checkout process.
+
+
+## Demo
+
+LINK
+
+## Features
+
+- Easy to integrate with Backend
+- Fully Responsive
+
+
+## Run Locally
+
+Clone the project
+
+Go to the project directory
+
+Install dependencies
+
+Start the server
+
+## Tech Stack
+
+* [React](https://reactjs.org/)
+* [Redux](https://redux.js.org/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Fake Store API](https://fakestoreapi.com/)
+
+## Contributing
+
+Contributions are always welcome!
+Just let me know what is missing and we'll try to figure how to fix that.
+
+## Feedback
+
+If you have any feedback, please reach out to me.
+
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        Welcome to Easy Bazar, the ultimate destination for a seamless and personalized online shopping experience! Our mission is to revolutionize the way you shop by providing an intuitive platform, tailored recommendations, and a hassle-free checkout process.
+
+Founded in 2023 by a team of passionate innovators, Easy Bazar was created to address the challenges faced by modern online shoppers. We understand the frustration of navigating complex websites, dealing with irrelevant product suggestions, and struggling with complicated checkouts. That's why we've made it our goal to simplify the shopping process and make it as enjoyable as possible.
+
+Our cutting-edge platform uses advanced algorithms to understand your preferences and provide personalized product recommendations based on your browsing history and interests. With an easy-to-navigate interface and a visually appealing design, Easy Bazar ensures a delightful shopping experience every time you visit.
+
+At Easy Bazar, we prioritize your security and privacy, implementing top-notch encryption and data protection measures. You can shop with confidence, knowing your information is safe with us. Our dedicated customer support team is always available to assist you with any queries or concerns.
+
+Join us on this exciting journey and discover a new world of online shopping that's tailored just for you. Welcome to Easy Bazar – where shopping is made easy!
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
